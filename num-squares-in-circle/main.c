18d95a189d832214148636d6727dd79f1b1a9e7d
Tushar Behera <trblinux@gmail.com>
2015-02-26 14:49:01 +0530

    num-squares-in-circle: Optimization methods added
    Test input now given through command line (as a file indirection).
    
    Signed-off-by: Tushar Behera <trblinux@gmail.com>

--- num-squares-in-circle/main.c
+++ num-squares-in-circle/main.c
@@ -5,11 +5,8 @@ static inline sqr(int i)
 	return i * i;
 }
 
-int main(int argc, char *argv[])
+int solve_problem_1(int r1, int r2)
 {
-	int r1;
-	int r2;
-
 	int i,j;
 	int num_squares = 0;
 	int d1, d2, d3, d4;
@@ -17,19 +14,8 @@ int main(int argc, char *argv[])
 	int min_dist;
 	int max_dist;
 
-	if (argc < 3) {
-		printf("Syntax: %s <small-circle-radius> <big-cirle-radius>\n", argv[0]);
-		r1 = 1;
-		r2 = 3;
-	} else {
-		r1 = atoi(argv[1]);
-		r2 = atoi(argv[2]);
-	}
-
 	min_dist = sqr(r1);
 	max_dist = sqr(r2);
-
-	printf("Finding number of unit squares between circles of radius %d and %d\n", r1, r2);
 
 	for (i = -1 * r2; i < r2; i++) {
 		for (j = -1 * r2; j < r2; j++) {
@@ -40,13 +26,84 @@ int main(int argc, char *argv[])
 			if (d1 >= min_dist && d2 >= min_dist && d3 >= min_dist && d4 >= min_dist) {
 				if (d1 <= max_dist && d2 <= max_dist && d3 <= max_dist && d4 <= max_dist) {
 					num_squares++;
-					printf("Unit square at (%d, %d) <-> (%d, %d)\n", i, j, i + 1, j + 1);
 				}
 			}
 		}
 	}
 
-	printf("Number of squares = %d\n", num_squares);
+	return num_squares;
+}
+
+int solve_problem_2(int r1, int r2)
+{
+	int i,j;
+	int num_squares = 0;
+	int d1, d2, d3, d4;
+
+	int min_dist;
+	int max_dist;
+
+	min_dist = sqr(r1);
+	max_dist = sqr(r2);
+
+	for (i = 0; i < r2; i++) {
+		for (j = 0; j < r2; j++) {
+			d1 = sqr(i) + sqr(j);
+			d2 = sqr(i + 1) + sqr(j + 1);
+			if (d1 >= min_dist && d2 <= max_dist)
+					num_squares++;
+		}
+	}
+
+	return 4 * num_squares;
+}
+
+int solve_problem_3(int r1, int r2)
+{
+	int i,j;
+	int num_squares = 0;
+	int d1, d2, d3, d4;
+
+	int min_dist;
+	int max_dist;
+
+	int lower_bound = r1 * 7 / 10;
+
+	min_dist = sqr(r1);
+	max_dist = sqr(r2);
+
+	for (i = 0; i < r2; i++) {
+		for (j = 0; j < r2; j++) {
+			if (i < lower_bound && j < lower_bound)
+				continue;
+			d1 = sqr(i) + sqr(j);
+			d2 = sqr(i + 1) + sqr(j + 1);
+			if (d1 >= min_dist && d2 <= max_dist)
+					num_squares++;
+		}
+	}
+
+	return 4 * num_squares;
+}
+
+
+int main(void)
+{
+	int num_test, n;
+	int r1, r2;
+	int num_squares;
+
+	scanf("%d", &num_test);
+
+	printf("Num test = %d\n", num_test);
+
+	for (n = 1; n <= num_test; n++) {
+		scanf("%d", &r1);
+		scanf("%d", &r2);
+		num_squares = solve_problem_3(r1, r2);
+		printf("Number-of-squares(%2d, %2d) = %d\n",
+				r1, r2, num_squares);
+	}
 
 	return 0;
 }
